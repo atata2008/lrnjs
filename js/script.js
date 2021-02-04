2838e198f421224dd6807cd75ed10462b96763eb
tatyana averkova <atata2008@mail.ru>
2021-02-04 21:55:46 +0300

    Finished 2th exersise of my coures js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt('What films did your watch?', '1');
+const numberOfFilms = +prompt('How many films did you watch?', '10?');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,7 +8,18 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const lastFilm = prompt('One from seen films?', 'Avatar?))');
-const gradeOfFilm = prompt('What grade is for this film?', '8.0');
-personalMovieDB.movies[lastFilm] = gradeOfFilm;
+if (personalMovieDB.count > 30) {
+    alert('Произошла ошибка');
+    } else if (personalMovieDB.count < 10) {
+    alert('Просмотрено мало фильмов');
+} else { alert('Вы классический зритель'); }
+
+for (let i = 0; i < 8; i++) {
+    const lastFilm = prompt('One from seen films?', '');
+    const gradeOfFilm = prompt('What grade is for this film?', '8.0');
+    if (lastFilm != null && lastFilm != '' && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = gradeOfFilm;
+        console.log('done');
+    } else { i--; }
+}
 console.log(personalMovieDB);
