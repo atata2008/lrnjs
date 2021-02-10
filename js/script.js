1bc2f8da94eefdff5637eb6cc572579e6bdf3cb6
tatyana averkova <atata2008@mail.ru>
2021-02-10 12:08:03 +0300

    Finished exercise №3 of my course js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-const numberOfFilms = +prompt('How many films did you watch?', '10?');
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt('How many films did you watch?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films did you watch?', '1?');
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,18 +17,48 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-if (personalMovieDB.count > 30) {
-    alert('Произошла ошибка');
-    } else if (personalMovieDB.count < 10) {
-    alert('Просмотрено мало фильмов');
-} else { alert('Вы классический зритель'); }
 
-for (let i = 0; i < 8; i++) {
-    const lastFilm = prompt('One from seen films?', '');
-    const gradeOfFilm = prompt('What grade is for this film?', '8.0');
-    if (lastFilm != null && lastFilm != '' && lastFilm.length < 50) {
-        personalMovieDB.movies[lastFilm] = gradeOfFilm;
-        console.log('done');
-    } else { i--; }
+function resumePersonalOfFilm() {
+    if (personalMovieDB.count > 30) {
+        alert('Произошла ошибка');
+        } else if (personalMovieDB.count < 10) {
+        alert('Просмотрено мало фильмов');
+    } else { alert('Вы классический зритель'); }
 }
-console.log(personalMovieDB);
+
+resumePersonalOfFilm();
+
+function personalInformationOfFilm() {
+    for (let i = 0; i < personalMovieDB.count && i < 3; i++) {
+        const lastFilm = prompt('One from seen films?', '');
+        const gradeOfFilm = prompt('What grade is for this film?', '8.0');
+        if (lastFilm != null && lastFilm != '' && lastFilm.length < 50) {
+            personalMovieDB.movies[lastFilm] = gradeOfFilm;
+            console.log('done');
+        } else { i--; }
+    }
+}
+
+personalInformationOfFilm();
+
+function writeYourGenres() {
+    let i = 0;
+        while (i < 3) {
+        let myGenre = prompt('What is your lovely genre №' + (i+1) + '?', '');
+        if (myGenre != null && myGenre != '') {
+            personalMovieDB.genres[i] = myGenre;
+        }
+        i++;
+    }
+
+}
+writeYourGenres();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
