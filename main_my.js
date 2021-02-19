09a41d0c614f5bdc7fb5282c8f64109f583f60ea
Averkova Tatyana <64754021+atata2008@users.noreply.github.com>
2021-02-19 12:33:21 +0300

    Add files via upload
    the done homework of this course

---
+++ main_my.js
@@ -0,0 +1,14 @@
+"use strict";
+// let arr = [];
+// function getSpecialNumbers(arr) {
+//     let i = 0, newArr = [], j = 0;
+//     for (i of arr) {
+//         if (arr[i] % 3 === 0) {
+//             newArr[j] = arr[i];
+//             j++;
+//         }
+//     }
+//     return newArr;
+// }
+// let myArr = [1, 2, 3, 5, 5, 56, 60, 99];
+// console.log(getSpecialNumbers(myArr));
